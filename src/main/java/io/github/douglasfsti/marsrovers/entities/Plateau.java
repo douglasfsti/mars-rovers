1ced111dcd44620cfaadef461f2d779ee8bdc958
Douglas Fernandes <douglassilva@quickmobile.com.br>
2016-12-21 22:13:54 -0200

    Entidade Plateau

--- src/main/java/io/github/douglasfsti/marsrovers/entities/Plateau.java
+++ src/main/java/io/github/douglasfsti/marsrovers/entities/Plateau.java
@@ -2,4 +2,24 @@ package io.github.douglasfsti.marsrovers.entities;
 
 public class Plateau {
 
+    private Coordinate coordinate;
+
+    public Plateau() {
+    }
+
+    public Plateau(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public int getSize() {
+        return this.coordinate.getX() - 1;
+    }
 }
